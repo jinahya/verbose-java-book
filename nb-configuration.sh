37058e8c3cfc6b97ae4c8d37b55a5b37da06937d
Jin Kwon <onacit@gmail.com>
2016-02-10 22:19:11 +0900

    updated

--- nb-configuration.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-find src/main/asciidoc/codes -mindepth 2 -name pom.xml -execdir pwd \; -execdir cp -v ../../../../../nb-configuration.xml . \;
-cp ./nb-configuration.xml ../verbose-java-benchmarks/
